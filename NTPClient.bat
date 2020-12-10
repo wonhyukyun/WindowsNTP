c2b1d2f2562526d382389bbfcab0171de0372261
Won Hyuk Yun <sbpapa@Won-ui-MacBook-Pro.local>
2020-12-10 16:31:16 +0900

    Add Client files

---
+++ NTPClient.bat
@@ -0,0 +1,6 @@
+@echo off 
+net start w32time w32tm /config /syncfromflags:manual /manualpeerlist:"%*" /reliable:yes /update 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient" /v "SpecialPollInterval" /t REG_DWORD /d 60 /f 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Config" /v "MaxPosPhaseCorrection" /t REG_DWORD /d 60 /f 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Config" /v "MaxNegPhaseCorrection" /t REG_DWORD /d 60 /f 
+net stop w32time net start w32time
